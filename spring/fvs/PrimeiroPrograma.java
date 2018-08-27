205476d4181b07377f5a38973d704e69686df7d7
jeffynlima <jeffersonfelipe1900@gmail.com>
2018-08-27 20:01:50 -0300

    Primeiro Programa Spring

---
+++ spring/fvs/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package fvs;
+
+public class PrimeiroPrograma {
+
+	public PrimeiroPrograma() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+	    System.out.println("Hello World!");
+
+	}
+
+}
