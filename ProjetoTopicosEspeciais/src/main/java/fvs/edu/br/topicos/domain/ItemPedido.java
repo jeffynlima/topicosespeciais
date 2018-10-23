106c17a08e5f2a3a4ef64cdd2a38bfdc9f96d3a7
jeffynlima <jeffersonfelipe1900@gmail.com>
2018-10-23 21:06:02 -0200

    Implementação do relacionamento entre pedido e produto

---
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/ItemPedido.java
@@ -0,0 +1,123 @@
+package fvs.edu.br.topicos.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class ItemPedido implements Serializable{
+	private static final long serialVersionUID = 1L; 
+
+	@JsonIgnore
+	@EmbeddedId
+	private ItemPedidoPK id = new ItemPedidoPK();
+	
+	private Double desconto;
+	private Double preco;
+	private Integer quantidade;
+	
+	public ItemPedido() {
+		
+	}
+
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Double preco, Integer quantidade) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.desconto = desconto;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public Produto getProduto() {
+		return id.getProduto();
+		
+	}
+	
+	@JsonIgnore
+	public Pedido getPedido() {
+		return id.getPedido();
+		
+	}
+	
+	public ItemPedidoPK getId() {
+		return id;
+	}
+
+	public void setId(ItemPedidoPK id) {
+		this.id = id;
+	}
+
+	public Double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(Double desconto) {
+		this.desconto = desconto;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((desconto == null) ? 0 : desconto.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+		result = prime * result + ((quantidade == null) ? 0 : quantidade.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		if (desconto == null) {
+			if (other.desconto != null)
+				return false;
+		} else if (!desconto.equals(other.desconto))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (preco == null) {
+			if (other.preco != null)
+				return false;
+		} else if (!preco.equals(other.preco))
+			return false;
+		if (quantidade == null) {
+			if (other.quantidade != null)
+				return false;
+		} else if (!quantidade.equals(other.quantidade))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
