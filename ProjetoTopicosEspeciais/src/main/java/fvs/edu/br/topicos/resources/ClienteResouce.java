36d66c547eed015e30967735c5dbbdbb7589eb18
jeffynlima <jeffersonfelipe1900@gmail.com>
2018-11-06 21:04:02 -0200

    Implementação Recurso e Serviço e Pedido

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/ClienteResouce.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/ClienteResouce.java
@@ -1,12 +1,31 @@
 package fvs.edu.br.topicos.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import fvs.edu.br.topicos.domain.Cliente;
+import fvs.edu.br.topicos.services.ClienteService;
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value = "/cliente")
 public class ClienteResouce {
 	
+	@Autowired
+	private ClienteService service;
 	
-
+	@RequestMapping(value = "/{id}",
+			method = RequestMethod.GET)
+	public ResponseEntity<?> find
+	(@PathVariable Integer id) throws ObjectNotFoundException{
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+	
+	
 }
